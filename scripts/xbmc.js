12bdd959411971c8eea15b006f08c0d4c27fc03a
Dan Nuttall <pixelblend@gmail.com>
2011-05-28 23:35:41 +0100

    Clean up XBMC model

--- scripts/xbmc.js
+++ scripts/xbmc.js
@@ -1,5 +1,11 @@
-var Xbmc = Backbone.Model.extend({
+var Xbmc = {
 		_connect: function(method, successCallback, params){
+			if (typeof params == 'object') {
+				jsonParams = JSON.stringify(params);
+			} else {
+				jsonParams = '{}';
+			}
+
 			$.ajax({
 					type: 'POST'
 				, username: 'xbmc'
@@ -8,19 +14,21 @@ var Xbmc = Backbone.Model.extend({
 				, url: 'http://xbmc.milo:8080/jsonrpc'
 				, cache: false
 				, dataType: 'json'
-				, data: '{"jsonrpc": "2.0", "method": "'+method+'", "params": '+JSON.stringify(params)+', "id": 1}'
+				, data: '{"jsonrpc": "2.0", "method": "'+method+'", "params": '+jsonParams+', "id": 1}'
 				,	error: function(jqXHR, textStatus, errorThrown){ alert(textStatus+' - '+errorThrown); }
 				, success: function(response){
-						if (successCallback === undefined){
-							return response.result;
-						} else {
+						if (typeof successCallback == 'function'){
 							successCallback(response.result);
+						} else {
+							return alert(JSON.stringify(response.result));
 						}
 					}
  			});
 		}
 	,	checkConnection: function(){
-			return this._connect('JSONRPC.Version');
+			this._connect('JSONRPC.Version', function(result){
+				$('<h1>XBMC Version '+result.version+'</h1>').appendTo('body');
+			});
 		}
 	,	listArtists: function(){
 		this._connect('AudioLibrary.GetArtists', function(result){
@@ -29,4 +37,6 @@ var Xbmc = Backbone.Model.extend({
       });
 		}, { "start": 0, "sort": { "order": "descending", "method": "artist" } });
 	}
-});
+};
+
+window.xbmc = Xbmc;
