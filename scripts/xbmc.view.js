0f45c47a592c844ef7bd96e7d9488c2296920576
Dan Nuttall <pixelblend@gmail.com>
2011-07-04 11:49:35 +0100

    Remove cruft from popup

--- scripts/xbmc.view.js
+++ scripts/xbmc.view.js
@@ -5,12 +5,10 @@ xbmc.view = {
 		controls: function(){
 			this._canvas();
 			this._buttons();
-			this._linkToIndex();
 			this._state();
 		}
 	,	_canvas: function(){
 			$('<div/>', {id: 'controls'}).appendTo('body');
-			$('<div/>', {id: 'state', text: 'Loading...'}).appendTo('body');
 		}
 	,	_buttons: function(){
 			var buttons = {'prev': 'Back', 'stop': 'Stop', 'play-pause':'Play/Pause', 'forward': 'Forward'};
@@ -23,11 +21,7 @@ xbmc.view = {
 	    if(typeof state == 'undefined'){
 	      return;
 	    }
-			$('#state').text(state);
 			$('#play-pause').text(state == 'stopped' ? 'Play' : 'Pause');
 			$('<h3/>', {text: localStorage.playing}).appendTo('body');
-		}
-	,	_linkToIndex: function(){
-			$('<a/>', {onClick: "chrome.tabs.create({url: 'index.html'})", text: 'Index', href: "#"}).appendTo('body');
 		}
 };
