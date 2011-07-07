662e679cef4a5951993b7c5fede9268a1a66c030
Dan Nuttall <pixelblend@gmail.com>
2011-07-07 13:52:14 +0100

    Remove unnecessary xbmc cruft around port object

--- scripts/xbmc.port.js
+++ scripts/xbmc.port.js
@@ -1,12 +1,6 @@
-if(typeof window.xbmc == 'undefined'){
-  window.xbmc = {};
-}
-
 port = chrome.extension.connect({name: "playlist"});
 chrome.extension.onConnect.addListener(function(port) {
   port.onMessage.addListener(function(msg) {
     xbmc.controller[msg]();
   });
 });
-
-xbmc.view = port;
