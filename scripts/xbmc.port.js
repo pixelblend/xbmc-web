35277bcb67db06c396105d369af716e7517cacec
Dan Nuttall <pixelblend@gmail.com>
2011-07-07 14:06:08 +0100

    Move pub/sub of ports into port object

--- scripts/xbmc.port.js
+++ scripts/xbmc.port.js
@@ -1,6 +1,18 @@
-port = chrome.extension.connect({name: "playlist"});
-chrome.extension.onConnect.addListener(function(port) {
-  port.onMessage.addListener(function(msg) {
-    xbmc.controller[msg]();
-  });
-});
+if(typeof window.xbmc == 'undefined'){
+  window.xbmc = {};
+}
+
+xbmc.port = {
+    name: 'playlist'
+  , subscribe: function(){
+      port = chrome.extension.connect({name: this.name});
+      chrome.extension.onConnect.addListener(function(port) {
+        port.onMessage.addListener(function(msg) {
+          xbmc.controller[msg]();
+        });
+      });
+    }
+  , publish: function(){
+      xbmc.controller = chrome.extension.connect({name: this.name});
+    }
+}
